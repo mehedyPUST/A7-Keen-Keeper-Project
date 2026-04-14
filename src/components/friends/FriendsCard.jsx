2f913e5100aabd574e7099847637ce50f7adba5d
mehedyPUST <mehedy.pust@gmail.com>
2026-04-14 16:54:55 +0600

    friends details section partially2

--- src/components/friends/FriendsCard.jsx
+++ src/components/friends/FriendsCard.jsx
@@ -3,6 +3,13 @@ import { Link } from 'react-router';
 
 const FriendsCard = ({ friend }) => {
     const { tags, status, days_since_contact, email, picture, name, id } = friend;
+    const formatStatus = (status) => {
+        return status
+            .split('-')
+            .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+            .join('-');
+    };
+
     return (
         <Link to={`/friend-details/${friend.id}`} className=' bg-white rounded-xl shadow-xl flex flex-col items-center p-4'>
             <img className='w-20 h-20 rounded-full' src={picture} alt={name} />
@@ -12,6 +19,8 @@ const FriendsCard = ({ friend }) => {
             <div className='flex gap-2 items-center'>
                 {tags.map(tag => <p className='badge badge-success'>{tag}</p>)}
             </div>
+
+            <p className='badge'>{formatStatus(status)}</p>
 
 
         </Link>
