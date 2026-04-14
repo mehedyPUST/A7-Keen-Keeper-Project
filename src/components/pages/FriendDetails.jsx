e42a196114233cb282145e60b75c40161f65588b
mehedyPUST <mehedy.pust@gmail.com>
2026-04-14 22:34:12 +0600

    history added not full func

--- src/components/pages/FriendDetails.jsx
+++ src/components/pages/FriendDetails.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CiVideoOn } from 'react-icons/ci';
 import { FiPhoneCall } from 'react-icons/fi';
 import { MdOutlineTextsms } from 'react-icons/md';
 import { useLoaderData, useParams } from 'react-router';
+import { toast } from 'react-toastify';
 
 const FriendDetails = () => {
     const { friendId } = useParams()
@@ -21,6 +22,24 @@ const FriendDetails = () => {
             year: "numeric"
         });
     }
+    const [history, setHistory] = useState([]);
+
+    const interactionHandlerVoiceCall = () => {
+        toast.success(`You Called ${expectedFriend.name}`);
+        setHistory(previous => [...previous, expectedFriend])
+        console.log(history, "history theke")
+    }
+    const interactionHandlerVideoCall = () => {
+        toast.success(`You Made Video Call With ${expectedFriend.name}`);
+        setHistory(previous => [...previous, expectedFriend])
+    }
+
+    const interactionHandlerTextMsg = () => {
+        toast.success(`Text Sent to  ${expectedFriend.name}`);
+        setHistory(previous => [...previous, expectedFriend])
+    }
+
+
 
     return (
         <div className='grid grid-cols-9 gap-4 w-11/12 mx-auto '>
@@ -84,20 +103,35 @@ const FriendDetails = () => {
                     <h3>Quick Check-In</h3>
                     <div className='w-full bg-amber-100 p-4  grid grid-cols-3 mt-4 gap-4'>
 
-                        <button className='bg-gray-50 rounded-md shadow-md flex flex-col items-center justify-center p-5 cursor-pointer space-y-2'>
+                        <button onClick={interactionHandlerVoiceCall} className='bg-gray-50 rounded-md shadow-md flex flex-col items-center justify-center p-5 cursor-pointer space-y-2'>
                             <FiPhoneCall className='text-xl' />
                             <p>Call</p>
                         </button>
-                        <button className='bg-gray-50 rounded-md shadow-md flex flex-col items-center justify-center p-5 cursor-pointer space-y-2'>
+                        <button onClick={interactionHandlerTextMsg} className='bg-gray-50 rounded-md shadow-md flex flex-col items-center justify-center p-5 cursor-pointer space-y-2'>
                             <MdOutlineTextsms className='text-xl' />
                             <p>Text</p>
                         </button>
-                        <button className='bg-gray-50 rounded-md shadow-md flex flex-col items-center justify-center p-5 cursor-pointer space-y-2'>
+                        <button onClick={interactionHandlerVideoCall} className='bg-gray-50 rounded-md shadow-md flex flex-col items-center justify-center p-5 cursor-pointer space-y-2'>
                             <CiVideoOn className='text-xl' />
                             <p>Video</p>
                         </button>
 
 
+
+                    </div>
+                </div>
+
+
+                <div className='w-full bg-white shadow-md p-4 mt-4 mb-4 rounded-md '>
+                    <div className='flex items-center justify-between'>
+                        <p className='font-semibold'>Recent Interactions </p>
+                        <button className='btn'>Full History</button>
+                    </div>
+
+                    <div className=' p-3 shadow-sm space-y-4 mt-4'>
+                        {
+                            history.map(info => <p className='bg-white p-2 shadow-sm '>{info.name}</p>)
+                        }
 
                     </div>
                 </div>
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router'
 import { router } from './routes/Routes'
+import { ToastContainer } from 'react-toastify'
 
 
 
@@ -10,6 +11,8 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router}>
 
+
     </RouterProvider>
+    <ToastContainer />
   </StrictMode>,
 )
