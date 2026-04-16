ccf7ba3755665d42bab869d25d7de0fa65086d6c
mehedyPUST <mehedy.pust@gmail.com>
2026-04-16 09:29:35 +0600

    banner retouch

--- src/components/HomePageHeading/HomePageHeading.jsx
+++ src/components/HomePageHeading/HomePageHeading.jsx
@@ -3,7 +3,7 @@ import { FaPlus } from 'react-icons/fa';
 
 const HomePageHeading = () => {
     return (
-        <div className='w-11/12 mx-auto flex flex-col items-center space-y-2'>
+        <div className='w-11/12 mx-auto flex flex-col items-center space-y-2 mt-10 mb-10 md:mt-15 md:mb-15'>
             <h1 className='font-extrabold text-3xl'>Friends to Keep Close in Your Life</h1>
             <p className='text-gray-500'>Your personal shelf of meaningful connections. Browse, tend, and nurture the
                 relationships that matter most.</p>
